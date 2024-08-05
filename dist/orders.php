ae5877618e31ed51bb8571915c4db448831f7281
Thomas Hartmeier <thomas.hartmeier@gmail.com>
2024-08-05 17:52:41 +0200

    fix link, remove duplicate header

--- dist/orders.php
+++ dist/orders.php
@@ -88,10 +88,7 @@ include("connection.php");
                 <main>
                     <div class="container-fluid px-4">
                         <h1 class="mt-4">Bestellungen</h1>
-                        <ol class="breadcrumb mb-4">
-                            <li class="breadcrumb-item active">Offene Bestellungen</li>
-                        </ol>
-                        <p><a class="btn btn-primary" href="index.html"><i class="bi bi-cart-plus"></i> Neue Bestellung</a></p>
+                        <p><a class="btn btn-primary" href="neworder.php"><i class="bi bi-cart-plus"></i> Neue Bestellung</a></p>
                         <div class="card mb-4">
                             <div class="card-header">
                                 <i class="fas fa-table me-1"></i>
