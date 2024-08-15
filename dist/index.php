2869484e0317f5e44e70570e7cead98d4569d161
Thomas Hartmeier <thomas.hartmeier@gmail.com>
2024-08-15 14:14:39 +0200

    add clients table to dashboard

--- dist/index.php
+++ dist/index.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+
+include("connection.php");
+
+//if (!isset($_SESSION['username'])) {
+//    header("location:login.php");
+//}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -141,6 +151,75 @@
                                     </div>
                                     <div class="card-body"><canvas id="myBarChart" width="100%" height="40"></canvas></div>
                                 </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container-fluid px-4">
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <i class="fas fa-table me-1"></i>
+                                2024: Anzahl verkaufte Flaschen pro Kunde
+                            </div>
+                            <div class="card-body">
+                                <table id="datatablesSimple">
+                                    <thead>
+                                        <tr>
+                                            <th>Kunde seit</th>
+                                            <th>Name</th>
+                                            <th>Firma</th>
+                                            <th>Wiederverkäufer</th>
+                                            <th>E-mail</th>
+                                            <th>Telefon/Mobile</th>
+                                            <th>Adresse</th>
+                                            <th>Erstellt von</th>
+                                            <th>Notiz</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        $sql = "SELECT * FROM clients";
+                                        $query = mysqli_query($conn, $sql) or die("Could not run SQL query.");
+
+                                        while ($result = mysqli_fetch_assoc($query))
+                                        {
+                                            $res_date = $result['createDate'];
+                                            $res_prename = $result['prename'];
+                                            $res_lastname = $result['lastname'];
+                                            $res_company = $result['company'];
+                                            $res_isReseller = $result['isReseller'];
+                                            $res_email = $result['email'];
+                                            $res_phone = $result['phone'];
+                                            $res_address = $result['address'];
+                                            $res_billingAddress = $result['billingAddress'];
+                                            $res_userId = $result['userId'];
+                                            $res_notes = $result['notes'];
+
+                                            $res_billingAddressString = ($res_billingAddress == '') ? "" : "<br><b>Rechnungsadresse:</b> $res_billingAddress";
+
+                                            echo "<tr>\n";
+                                            echo "    <td>$res_date</td>\n";
+                                            echo "    <td>$res_prename $res_lastname</td>\n";
+                                            echo "    <td>$res_company</td>\n";
+
+                                            $resellerFlag = ($res_isReseller) ? "<i class=\"bi bi-star-fill\" style=\"color: #e8e004;\"></i>" : '';
+                                            echo "    <td>$resellerFlag</td>\n";
+                                            echo "    <td>$res_email</td>\n";
+                                            echo "    <td>$res_phone</td>\n";
+                                            echo "    <td>$res_address $res_billingAddressString</td>\n";
+
+                                            // get name for userId
+                                            $sqlUser = "SELECT * FROM users WHERE id = $res_userId";
+                                            $queryUser = mysqli_query($conn, $sqlUser) or die("Could not run SQL query.");
+                                            $resultUser = mysqli_fetch_assoc($queryUser);
+                                            $userName = $resultUser['prename'];
+                                            echo "    <td>$userName</td>\n";
+
+                                            echo "    <td>$res_notes</td>\n";
+                                            echo "</tr>\n";
+                                        }
+                                        ?>
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
