c9fcf855171f85b9da2ac7f5c20510e880c06697
Thomas Hartmeier <thomas.hartmeier@gmail.com>
2024-08-15 13:40:27 +0200

    do not need second table on same page anymore

--- dist/js/datatables-simple-demo.js
+++ dist/js/datatables-simple-demo.js
@@ -6,9 +6,4 @@ window.addEventListener('DOMContentLoaded', event => {
     if (datatablesSimple) {
         new simpleDatatables.DataTable(datatablesSimple);
     }
-
-    const datatablesSimple2 = document.getElementById('datatablesSimple2');
-    if (datatablesSimple2) {
-        new simpleDatatables.DataTable(datatablesSimple2);
-    }
 });
