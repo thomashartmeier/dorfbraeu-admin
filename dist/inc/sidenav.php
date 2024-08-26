0a3d69590c452be9b257ce565769056d03226b5e
Thomas Hartmeier <thomas.hartmeier@gmail.com>
2024-08-26 10:04:09 +0200

    change sidenav to php code

--- dist/inc/sidenav.php
+++ dist/inc/sidenav.php
@@ -1,40 +1,57 @@
-<nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-    <div class="sb-sidenav-menu">
-        <div class="nav">
-            <div class="sb-sidenav-menu-heading">Info</div>
-            <a class="nav-link" href="index.php">
-                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                Dashboard
-            </a>
-            <a class="nav-link" href="prices.php">
-                <div class="sb-nav-link-icon"><i class="bi bi-currency-dollar"></i></div>
-                Preisgestaltung
-            </a>
-            <div class="sb-sidenav-menu-heading">Edit</div>
-            <a class="nav-link" href="openorders.php">
-                <div class="sb-nav-link-icon"><i class="bi bi-cart-fill"></i></div>
-                Offene Bestellungen
-            </a>
-            <a class="nav-link" href="allorders.php">
-                <div class="sb-nav-link-icon"><i class="bi bi-cart-fill"></i></div>
-                Alle Bestellungen
-            </a>
-            <a class="nav-link" href="neworder.php">
-                <div class="sb-nav-link-icon"><i class="bi bi-plus-circle"></i></div>
-                Neue Bestellung
-            </a>
-            <a class="nav-link" href="clients.php">
-                <div class="sb-nav-link-icon"><i class="bi bi-file-earmark-person"></i></div>
-                Kunden
-            </a>
-            <a class="nav-link" href="newclient.php">
-                <div class="sb-nav-link-icon"><i class="bi bi-plus-circle"></i></div>
-                Neuer Kundeneintrag
-            </a>
-        </div>
-    </div>
-    <div class="sb-sidenav-footer">
-        <div class="small">Logged in as:</div>
-        Start Bootstrap
-    </div>
-</nav>
+<?php
+
+echo "<nav class=\"sb-sidenav accordion sb-sidenav-dark\" id=\"sidenavAccordion\">\n";
+echo "    <div class=\"sb-sidenav-menu\">\n";
+echo "        <div class=\"nav\">\n";
+echo "            <div class=\"sb-sidenav-menu-heading\">Info</div>\n";
+echo "            <a class=\"nav-link\" href=\"index.php\">\n";
+echo "                <div class=\"sb-nav-link-icon\"><i class=\"fas fa-tachometer-alt\"></i></div>\n";
+echo "                Dashboard\n";
+echo "            </a>\n";
+echo "            <a class=\"nav-link\" href=\"prices.php\">\n";
+echo "                <div class=\"sb-nav-link-icon\"><i class=\"bi bi-currency-dollar\"></i></div>\n";
+echo "                Preisgestaltung\n";
+echo "            </a>\n";
+echo "            <div class=\"sb-sidenav-menu-heading\">Bestellungen</div>\n";
+echo "            <a class=\"nav-link\" href=\"openorders.php\">\n";
+echo "                <div class=\"sb-nav-link-icon\"><i class=\"bi bi-cart-fill\"></i></div>\n";
+echo "                Offene Bestellungen\n";
+echo "            </a>\n";
+echo "            <a class=\"nav-link\" href=\"allorders.php\">\n";
+echo "                <div class=\"sb-nav-link-icon\"><i class=\"bi bi-cart-fill\"></i></div>\n";
+echo "                Alle Bestellungen\n";
+echo "            </a>\n";
+echo "            <a class=\"nav-link\" href=\"neworder.php\">\n";
+echo "                <div class=\"sb-nav-link-icon\"><i class=\"bi bi-plus\"></i></div>\n";
+echo "                Neue Bestellung\n";
+echo "            </a>\n";
+echo "            <div class=\"sb-sidenav-menu-heading\">Kunden</div>\n";
+echo "            <a class=\"nav-link\" href=\"clients.php\">\n";
+echo "                <div class=\"sb-nav-link-icon\"><i class=\"bi bi-file-earmark-person\"></i></div>\n";
+echo "                Alle Kunden\n";
+echo "            </a>\n";
+echo "            <a class=\"nav-link\" href=\"newclient.php\">\n";
+echo "                <div class=\"sb-nav-link-icon\"><i class=\"bi bi-plus\"></i></div>\n";
+echo "                Neuer Kundeneintrag\n";
+echo "            </a>\n";
+echo "            <div class=\"sb-sidenav-menu-heading\">Sude/Abfüllungen</div>\n";
+echo "            <a class=\"nav-link\" href=\"allbrews.php\">\n";
+echo "                <div class=\"sb-nav-link-icon\"><i class=\"bi bi-droplet-fill\"></i></div>\n";
+echo "                Alle Sude/Abfüllungen\n";
+echo "            </a>\n";
+echo "            <a class=\"nav-link\" href=\"newbrew.php\">\n";
+echo "                <div class=\"sb-nav-link-icon\"><i class=\"bi bi-plus\"></i></div>\n";
+echo "                Neuer Sud/Abfüllung\n";
+echo "            </a>\n";
+echo "        </div>\n";
+echo "    </div>\n";
+echo "    <div class=\"sb-sidenav-footer\">\n";
+echo "        <div class=\"small\">Logged in as:</div>\n";
+
+$prename = $_SESSION['prename'];
+echo "$prename\n";
+
+echo "    </div>\n";
+echo "</nav>\n";
+
+?>
