cb59b4355067e6d00cb3f9ae13a3c8954fecd9b5
Thomas Hartmeier <thomas.hartmeier@gmail.com>
2024-08-26 18:01:48 +0200

    new functions to get number of brewed and sold bottles

--- dist/index.php
+++ dist/index.php
@@ -3,7 +3,71 @@ session_start();
 
 include("connection.php");
 
-if (!isset($_SESSION['username'])) {
+function getBottlesRemaining($input_beerId)
+{
+    global $conn;
+
+    $bottlesBrewed = getBottlesBrewed($input_beerId);
+    $bottlesSold = getBottlesSold($input_beerId);
+
+    return $bottlesBrewed - $bottlesSold;
+}
+
+function getBottlesBrewed($input_beerId)
+{
+    global $conn;
+
+    $totalNumBottles = 0;
+
+    // loop over all brews of this beer type
+    $sql = "SELECT * FROM brews WHERE beerId = $input_beerId";
+    $query = mysqli_query($conn, $sql) or die("Could not run SQL query.");
+
+    while ($result = mysqli_fetch_assoc($query))
+    {
+        $res_numBottles = $result['numBottles'];
+
+        $totalNumBottles += $res_numBottles;
+    }
+
+    return $totalNumBottles;
+}
+
+function getBottlesSold($input_beerId)
+{
+    global $conn;
+
+    $totalNumBottles = 0;
+
+    // loop over all orders and sum the order items of this beer type
+    $sql = "SELECT * FROM orders";
+    $query = mysqli_query($conn, $sql) or die("Could not run SQL query.");
+
+    while ($result = mysqli_fetch_assoc($query))
+    {
+        $res_id = $result['id'];
+
+        // get all the order items that belong to that order
+        $sqlItem = "SELECT * FROM orderItems WHERE orderId = $res_id";
+        $queryItem = mysqli_query($conn, $sqlItem) or die("Could not run SQL query.");
+
+        while ($resultItem = mysqli_fetch_assoc($queryItem))
+        {
+            $orderItems_beerId = $resultItem['beerId'];
+            $orderItems_quantity = $resultItem['quantity'];
+
+            if ($orderItems_beerId == $input_beerId)
+            {
+                $totalNumBottles += $orderItems_quantity;
+            }
+        }
+    }
+
+    return $totalNumBottles;
+}
+
+if (!isset($_SESSION['username']))
+{
     header("location:login.php");
 }
 ?>
@@ -25,49 +89,63 @@ if (!isset($_SESSION['username'])) {
                             <li class="breadcrumb-item active">Dashboard</li>
                         </ol>
                         <div class="row">
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body">Primary Card</div>
+                            <div class="col-lg-2 col-md-6">
+                                <div class="card bg-frischesmais text-dark mb-4">
+                                    <div class="card-body"><b>Frisches Mais</b></div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                        <ul>
+                                            <li>Stock: <?php echo getBottlesRemaining(0); ?> Flaschen</li>
+                                            <li>Total gebraut: <?php echo getBottlesBrewed(0); ?> Flaschen</li>
+                                            <li>Total verkauft: <?php echo getBottlesSold(0); ?> Flaschen</li>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-warning text-white mb-4">
-                                    <div class="card-body">Warning Card</div>
+                            <div class="col-lg-2 col-md-6">
+                                <div class="card bg-vollesdinkel text-white mb-4">
+                                    <div class="card-body"><b>Volles Dinkel</b></div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                        <ul>
+                                            <li>Stock: <?php echo getBottlesRemaining(3); ?> Flaschen</li>
+                                            <li>Total gebraut: <?php echo getBottlesBrewed(3); ?> Flaschen</li>
+                                            <li>Total verkauft: <?php echo getBottlesSold(3); ?> Flaschen</li>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-success text-white mb-4">
-                                    <div class="card-body">Success Card</div>
+                            <div class="col-lg-2 col-md-6">
+                                <div class="card bg-funkyipa text-white mb-4">
+                                    <div class="card-body"><b>Funky IPA</b></div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                        <ul>
+                                            <li>Stock: <?php echo getBottlesRemaining(2); ?> Flaschen</li>
+                                            <li>Total gebraut: <?php echo getBottlesBrewed(2); ?> Flaschen</li>
+                                            <li>Total verkauft: <?php echo getBottlesSold(2); ?> Flaschen</li>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-xl-3 col-md-6">
-                                <div class="card bg-danger text-white mb-4">
-                                    <div class="card-body">Danger Card</div>
+                            <div class="col-lg-2 col-md-6">
+                                <div class="card bg-lovelyamber text-white mb-4">
+                                    <div class="card-body"><b>Lovely Amber</b></div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">View Details</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                        <ul>
+                                            <li>Stock: <?php echo getBottlesRemaining(4); ?> Flaschen</li>
+                                            <li>Total gebraut: <?php echo getBottlesBrewed(4); ?> Flaschen</li>
+                                            <li>Total verkauft: <?php echo getBottlesSold(4); ?> Flaschen</li>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-xl-6">
-                                <div class="card mb-4">
-                                    <div class="card-header">
-                                        <i class="fas fa-chart-area me-1"></i>
-                                        Area Chart Example
+                            <div class="col-lg-2 col-md-6">
+                                <div class="card bg-freakycraft text-white mb-4">
+                                    <div class="card-body"><b>Freaky Craft</b></div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <ul>
+                                            <li>Stock: <?php echo getBottlesRemaining(1); ?> Flaschen</li>
+                                            <li>Total gebraut: <?php echo getBottlesBrewed(1); ?> Flaschen</li>
+                                            <li>Total verkauft: <?php echo getBottlesSold(1); ?> Flaschen</li>
+                                        </ul>
                                     </div>
                                     <div class="card-body"><canvas id="myAreaChart" width="100%" height="40"></canvas></div>
                                 </div>
